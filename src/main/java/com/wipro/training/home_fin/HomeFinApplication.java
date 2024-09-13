e8906b2c4b7feed7659919e985f085522a16150a
nagavenkatallu <venkatallu97@gmail.com>
2024-09-12 23:58:05 -0500

    initial commit

---
+++ src/main/java/com/wipro/training/home_fin/HomeFinApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.training.home_fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wipro/training/home_fin/HomeFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.training.home_fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
